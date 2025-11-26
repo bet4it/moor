b08759c0d4b5c4c4a6259a89ae225023edc16719
Johan Walles <johan.walles@gmail.com>
2025-11-26 21:34:00 +0100

    Add range locking logic

--- internal/reader/locking.go
+++ internal/reader/locking.go
@@ -1,17 +1,59 @@
 package reader
 
-func (reader *ReaderImpl) rLock() {
-	reader.lock.RLock()
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+const LOCK_SECTION_SIZE = uint64(1024)
+
+func createLocks() []sync.RWMutex {
+	numLocks := runtime.NumCPU() * 4
+	locks := make([]sync.RWMutex, numLocks)
+	return locks
 }
 
-func (reader *ReaderImpl) rUnlock() {
-	reader.lock.RUnlock()
+func (reader *ReaderImpl) getLocksForRange(startIndexInclusive uint64, endIndexInclusive uint64) []*sync.RWMutex {
+	if endIndexInclusive < startIndexInclusive {
+		panic(fmt.Sprintf("Can't lock negative size range %d-%d", startIndexInclusive, endIndexInclusive))
+	}
+
+	lockIndex := (startIndexInclusive / LOCK_SECTION_SIZE) % uint64(len(reader.locks))
+	lastLockIndex := (endIndexInclusive / LOCK_SECTION_SIZE) % uint64(len(reader.locks))
+
+	var locksToUse []*sync.RWMutex
+	for {
+		locksToUse = append(locksToUse, &reader.locks[lockIndex])
+		if lockIndex == lastLockIndex {
+			break
+		}
+		lockIndex = (lockIndex + 1) % uint64(len(reader.locks))
+	}
+
+	return locksToUse
 }
 
-func (reader *ReaderImpl) rwLock() {
-	reader.lock.Lock()
+func (reader *ReaderImpl) rLock(startIndexInclusive uint64, endIndexInclusive uint64) {
+	for _, lock := range reader.getLocksForRange(startIndexInclusive, endIndexInclusive) {
+		lock.RLock()
+	}
 }
 
-func (reader *ReaderImpl) rwUnlock() {
-	reader.lock.Unlock()
+func (reader *ReaderImpl) rUnlock(startIndexInclusive uint64, endIndexInclusive uint64) {
+	for _, lock := range reader.getLocksForRange(startIndexInclusive, endIndexInclusive) {
+		lock.RUnlock()
+	}
+}
+
+func (reader *ReaderImpl) rwLock(startIndexInclusive uint64, endIndexInclusive uint64) {
+	for _, lock := range reader.getLocksForRange(startIndexInclusive, endIndexInclusive) {
+		lock.Lock()
+	}
+}
+
+func (reader *ReaderImpl) rwUnlock(startIndexInclusive uint64, endIndexInclusive uint64) {
+	for _, lock := range reader.getLocksForRange(startIndexInclusive, endIndexInclusive) {
+		lock.Unlock()
+	}
 }
